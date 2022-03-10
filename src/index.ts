f7bad1130e63f4078858cffcb02432c2554e646f
zg.xie <zg.xie@huolala.cn>
2022-03-10 19:40:17 +0800

    fix: remote detailInfo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export type { ButtonProps } from "./components/button";
 export { default as Button } from "./components/button";
-export { default as DetailInfo } from "./components/detailInfo";
 export * from "./hooks";
 export * from "./utils";
