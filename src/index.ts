9d91d265bb3391169b3b6d0edbb1e66e69f40032
zg.xie <zg.xie@huolala.cn>
2022-03-10 19:51:35 +0800

    feat: fix export bug

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export type { ButtonProps } from "./components/button";
 export { default as Button } from "./components/button";
+export { default as ModalCascader } from "./components/modal-cascader";
 export * from "./hooks";
 export * from "./utils";
